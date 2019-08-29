f1dfac898ec7930e1ffa515dc45ac398505bec1f
Christine Laptop <Christine Laptop@DESKTOP-SSF87HE>
2019-08-29 16:21:10 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM4300MYFIRSTAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first console app");
+        }
+    }
+}
